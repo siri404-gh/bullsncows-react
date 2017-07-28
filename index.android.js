5bc10bf36be0d685872e872a8d11898821751e01
Sreeram Padmanabhan <sreeram.com@gmail.com>
2017-07-28 15:32:19 +0100

    updates

--- index.android.js
+++ index.android.js
@@ -17,7 +17,7 @@ export default class reactUltimate extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          React Ultimate..
+          React Ultimat..
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
