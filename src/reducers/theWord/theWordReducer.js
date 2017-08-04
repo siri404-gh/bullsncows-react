d01238344244921768067dab96d7d3fcdb7f80d9
Sreeram Padmanabhan <sreeram.com@gmail.com>
2017-08-04 06:54:57 +0100

    testcases

--- src/reducers/theWord/theWordReducer.js
+++ src/reducers/theWord/theWordReducer.js
@@ -1,11 +1,11 @@
 import word from '../words';
-var w = word();
-const theWordReducer = (state = w, action) => {
+
+const theWordReducer = (state = word(), action) => {
   switch (action.type) {
     case 'ADD_NEW_WORD':
       return word();
     default:
-      return w;
+      return state;
   }
 };
 
