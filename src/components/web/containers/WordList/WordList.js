b61332b6eb7d91f91a10546901169bafeb9ca53c
Sreeram Padmanabhan <sreeram.com@gmail.com>
2017-08-11 22:02:33 +0100

    updates

--- src/components/web/containers/WordList/WordList.js
+++ src/components/web/containers/WordList/WordList.js
@@ -8,7 +8,6 @@ import { addNewWord } from '../../../../actions/theWord/theWordActions';
 import { addLastWord } from '../../../../actions/lastword/lastwordActions';
 import { getUsers } from '../../../../actions/users/usersActions';
 import bulls from '../../app/bulls.png';
-import bullsncows from '../../app/bullsncows.png';
 
 import Styles from './WordList.less';
 
@@ -61,7 +60,7 @@ const mapDispatchToProps = (dispatch) => ({
       method: 'feed',
       link: 'https://reactultimate-firebase.herokuapp.com/',
       caption: 'I scored ' + num + ' points.',
-      picture: bullsncows,
+      picture: './bullsncows.png',
       thumbnail: bulls
     }, function(response){});
 
